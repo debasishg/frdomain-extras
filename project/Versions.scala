faadd0a87f6a244e83f8dc1167b3ba3ae64df775
Debasish Ghosh <ghosh.debasish@gmail.com>
2021-10-04 19:24:24 +0530

    Removed conflicts

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,13 +1,7 @@
 object Versions {
-<<<<<<< HEAD
   val catsVersion = "2.2.0"
   val catsEffectVersion = "2.1.4"
-  val catsMtlVersion = "0.7.0"
-=======
-  val catsVersion = "2.2.0-M2"
-  val catsEffectVersion = "2.1.3"
   val catsMtlVersion = "0.7.1"
->>>>>>> 05b7152ee0100864445227b0a80c7c856ed2b55c
   val macroParadiseVersion = "2.1.1"
   val kindProjectorVersion = "0.13.2"
   val monixVersion = "3.2.2"
@@ -15,23 +9,12 @@ object Versions {
   val zioVersion = "1.0.12"
   val zioLoggingVersion = "0.2.9"
   val silencerVersion = "1.4.4"
-<<<<<<< HEAD
   val pureconfigVersion = "0.12.3"
   val log4j2Version = "2.13.3"
   val doobieVersion = "0.13.4"
   val newtypeVersion = "0.4.4"
   val refinedVersion = "0.9.27"
-  val squantsVersion = "1.6.0"
+  val squantsVersion = "1.8.3"
   val skunkVersion = "0.0.28"
   val cirisVersion = "1.1.2"
-=======
-  val log4j2Version = "2.13.1"
-  val pureconfigVersion = "0.12.1"
-  val doobieVersion = "0.8.8"
-  val newtypeVersion = "0.4.3"
-  val refinedVersion = "0.9.14"
-  val squantsVersion = "1.8.3"
-  val skunkVersion = "0.0.11"
-  val cirisVersion = "1.1.0"
->>>>>>> 3325ff7328e752687a7039060014c93cdb1346a1
 }
