4b4f9b63ea3ffdabad13b6926da308e97cb849d7
Scala Steward <me@scala-steward.org>
2021-11-15 12:16:39 +0100

    Update ciris, ciris-enumeratum, ... to 2.2.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -17,6 +17,6 @@ object Versions {
   val refinedVersion = "0.9.27"
   val squantsVersion = "1.8.3"
   val skunkVersion = "0.2.2"
-  val cirisVersion = "2.2.0"
+  val cirisVersion = "2.2.1"
   val flywayDbVersion = "8.0.4"
 }
