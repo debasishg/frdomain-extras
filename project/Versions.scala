bd93c7a1724004ab69116ef743ab846f5e6abfa9
Scala Steward <me@scala-steward.org>
2021-11-22 20:25:20 +0100

    Update zio-interop-cats to 3.2.9.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,7 +8,7 @@ object Versions {
   val scalaVersion = "2.13.7"
   val zioVersion = "1.0.12"
   val zioLoggingVersion = "0.5.14"
-  val zioInteropCatsVersion = "3.1.1.0"
+  val zioInteropCatsVersion = "3.2.9.0"
   val silencerVersion = "1.4.4"
   val pureconfigVersion = "0.17.1"
   val log4j2Version = "2.14.1"
